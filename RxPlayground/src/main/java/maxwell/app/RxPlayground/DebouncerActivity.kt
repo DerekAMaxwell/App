ee7ca1eb703fdd77850fac7f1adbdb826fe5acdc
Derek Maxwell <DerekAMaxwell@gmail.com>
2018-10-27 11:04:36 -0700

    Finished Debouncers
    Added the debouncer with timer and a debouncer with RxJava.

--- RxPlayground/src/main/java/maxwell/app/RxPlayground/DebouncerActivity.kt
+++ RxPlayground/src/main/java/maxwell/app/RxPlayground/DebouncerActivity.kt
@@ -6,22 +6,34 @@ import android.text.Editable
 import android.text.TextWatcher
 import kotlinx.android.synthetic.main.activity_debouncer.*
 import org.w3c.dom.Text
+import java.util.*
 
 class DebouncerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debouncer)
+        var timer = Timer()
 
         editText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                textView.text = editText.text
+                timer = Timer()
+                // After a second has passed, set the text field's value to be the text that the user entered
+                timer.schedule(
+                    object: TimerTask() {
+                        override fun run() {
+                            runOnUiThread { textView.text = editText.text }
+                        }
+                    },
+                    1000
+                )
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
+                // Reset the timer
+                timer.cancel()
             }
         })
     }
--- RxPlayground/src/main/java/maxwell/app/RxPlayground/RxDebouncerActivity.kt
+++ RxPlayground/src/main/java/maxwell/app/RxPlayground/RxDebouncerActivity.kt
@@ -4,23 +4,26 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
+import io.reactivex.Observable
+import io.reactivex.ObservableOnSubscribe
 import kotlinx.android.synthetic.main.activity_debouncer.*
+import java.util.concurrent.TimeUnit
 
 class RxDebouncerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debouncer)
 
-        editText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                textView.text = editText.text
-            }
+        Observable.create(ObservableOnSubscribe<String> { subscriber ->
+            editText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-        })
+                override fun afterTextChanged(s: Editable?) = subscriber.onNext(s.toString())
+            })
+        }).debounce(1000, TimeUnit.MILLISECONDS).subscribe {
+            textView.text = it
+        }
     }
 }
